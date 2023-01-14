6f02dc9b220649425401162cb73754f0291ded1b
Alistair Houghton <alistair.g.houghton@gmail.com>
2023-01-14 23:20:05 +0000

    added initial files

---
+++ toggle.js
@@ -0,0 +1,33 @@
+const toggle = document.getElementById("toggle");
+const sections = document.getElementsByClassName("section");
+const lights = document.getElementsByClassName("light");
+const lightsSvg = document.getElementsByClassName("light-svg");
+const setNav = document.getElementById("nav-overlay");
+
+toggleHandler = () => {
+  if (toggle.checked === false) {
+    sections[0].setAttribute("data-bg", "#23b296");
+    sections[1].setAttribute("data-bg", "#f15f61");
+    sections[2].setAttribute("data-bg", "#DDBCF7");
+    sections[3].setAttribute("data-bg", "#fca971");
+    setNav.style.backgroundColor = "#23b296";
+  }
+  if (toggle.checked === true) {
+    sections[0].setAttribute("data-bg", "#1A1C52");
+    sections[1].setAttribute("data-bg", "#1A1C52");
+    sections[2].setAttribute("data-bg", "#1A1C52");
+    sections[3].setAttribute("data-bg", "#1A1C52");
+    setNav.style.backgroundColor = "#1A1C52";
+  }
+
+  onScroll();
+  [...lights].forEach((el) => {
+    el.classList.toggle("dark");
+  });
+
+  [...lightsSvg].forEach((el) => {
+    el.classList.toggle("dark-svg");
+  });
+};
+
+toggle.addEventListener("click", toggleHandler);
